732f562be0f8a5f7d6f7a9637bdae93a7cf2bff3
keolaj <keolajarvegren@gmail.com>
2019-04-12 01:35:02 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine = Engine.create();
+
